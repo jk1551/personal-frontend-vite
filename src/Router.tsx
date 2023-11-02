996ef0845b98c128c4f942ff3ed230778e0686d5
Joe King <joe.k659@gmail.com>
2023-11-02 17:43:25 -0400

    first commit to core

---
+++ src/Router.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './pages/Error.page';
+import { HomePage } from './pages/Home.page';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '*',
+    element: <ErrorPage />,
+  },
+]);
+
+export function Router() {
+  return <RouterProvider router={router} />;
+}
